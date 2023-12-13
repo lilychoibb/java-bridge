cc0cc7dc164907f42d1c3b28ca4bd84bff10649a
yoonseo choi <choiyoonseo02@gmail.com>
2023-12-13 19:54:25 +0900

    feat: 게임 진행 로직

--- src/main/java/bridge/domain/Controller.java
+++ src/main/java/bridge/domain/Controller.java
@@ -1,17 +1,22 @@
 package bridge.domain;
 
+import bridge.BridgeGame;
 import bridge.BridgeMaker;
+import bridge.model.BridgeSize;
+import bridge.model.Judgement;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
-    private InputView inputView;
-    private OutputView outputView;
-    private BridgeMaker bridgeMaker;
+    private final OutputView outputView;
+    private final InputView inputView;
+    private final BridgeMaker bridgeMaker;
 
-    public Controller(InputView inputView, OutputView outputView, BridgeMaker bridgeMaker) {
-        this.inputView = inputView;
+    public Controller(OutputView outputView, InputView inputView, BridgeMaker bridgeMaker) {
         this.outputView = outputView;
+        this.inputView = inputView;
         this.bridgeMaker = bridgeMaker;
     }
 
@@ -19,8 +24,39 @@ public class Controller {
         outputView.startGameMessage();
         System.out.println();
 
-        int bridgeSize = inputView.readBridgeSize();
-        bridgeMaker.makeBridge(bridgeSize);
+        BridgeSize bridgeSize = inputBridgeSize();
+
+        List<String> resultBridge = bridgeMaker.makeBridge(bridgeSize.getBridgeSize());
+
+        List<List<String>> result = new ArrayList<>();
+
+        Judgement judgement = new Judgement(result);
+        int gameTry = gameLoop(resultBridge, judgement);
+    public int gameLoop(List<String> resultBridge, Judgement judgement) {
+        for (int i = 0; i < resultBridge.size(); i++) {
+            BridgeGame bridgeGame = inputBridgeMoving();
+            BridgeGame.move();
+
+            boolean gameResult = judgement.gameResult(resultBridge, bridgeGame);
+
+            if (gameResult) {
+                outputView.printMap(judgement);
+            }
+
+            if (!gameResult) {
+                outputView.printMap(judgement);
+                boolean retry = inputRetry(bridgeGame);
+                if (retry) {
+                    return 1;
+                }
+                outputView.printResult(judgement);
+                return 2;
+            }
+        }
+        outputView.printResult(judgement);
+        return 3;
+    }
+
     private BridgeSize inputBridgeSize() {
         try {
             int bridgeSize = inputView.readBridgeSize();

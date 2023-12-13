c3341269f5418705b90326e8a740df0fc05b0ae7
yoonseo choi <choiyoonseo02@gmail.com>
2023-12-13 20:02:15 +0900

    feat: 플레이어가 이동한 칸 및 횟수 저장

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,13 +4,23 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final String moving;
+    private static int movingCount;
+
+    public BridgeGame(String moving) {
+        validator(moving);
+        this.moving = moving;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public static void move() {
+        movingCount += 1;
+    }
+
     }
 
     /**
